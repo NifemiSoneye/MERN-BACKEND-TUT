2fac043b5004ff5933ddaf241fb22cd6f0287af8
NifemiSoneye <nifemisoneye@gmail.com>
2026-03-19 07:47:39 -0700

    Cookie parser added

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -2,7 +2,8 @@ const { logEvents } = require("./logEvents");
 const errorHandler = (err, req, res, next) => {
   logEvents(`${err.name} : ${err.message}`, "errLog.txt");
   console.error(err.stack);
-  res.status(500).send(err.message);
+  const status = res.statusCode ? res.statusCode : 500;
+  res.status(status);
 };
 
 module.exports = errorHandler;
