69203cfeba437d2ed2cd33eda2ec026f865b93e0
NifemiSoneye <nifemisoneye@gmail.com>
2026-03-19 07:56:46 -0700

    Initialize cors and created allowed origins

--- server.js
+++ server.js
@@ -5,8 +5,11 @@ const path = require("path");
 const { logger } = require("./middleware/logger");
 const errorHandler = require("./middleware/errorHandler");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 app.use(logger);
+
+app.use(cors());
 
 app.use(express.json());
 
