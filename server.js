800fc92b6e06c1ae38775d369bf97964757d7c69
NifemiSoneye <nifemisoneye@gmail.com>
2026-03-20 04:06:01 -0700

    mongoose listeners added to server

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-require("dotenv").config;
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3500;
@@ -8,8 +8,13 @@ const errorHandler = require("./middleware/errorHandler");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const corsOptions = require("./config/corsOptions");
+const connectDB = require("./config/dbConn");
+const mongoose = require("mongoose");
+const { logEvents } = require("./middleware/logger");
 
 console.log(process.env.NODE_ENV);
+
+connectDB();
 app.use(logger);
 
 app.use(cors(corsOptions));
@@ -35,4 +40,15 @@ app.all(/.*/, (req, res) => {
 
 app.use(errorHandler);
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+mongoose.connection.once("open", () => {
+  console.log("Connected to MongoDB");
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+});
+
+mongoose.connection.on("error", (err) => {
+  console.log(err);
+  logEvents(
+    `${err.no}: ${err.code}\t ${err.syscall}\t${err.hostname}`,
+    "mongoErrLog.log",
+  );
+});
