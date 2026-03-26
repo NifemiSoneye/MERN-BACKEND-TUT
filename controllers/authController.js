59dfe061eb33e1c364d53d4e9932afdb5dd04283
NifemiSoneye <nifemisoneye@gmail.com>
2026-03-26 11:46:12 -0700

    verifiers added to routes

--- controllers/authController.js
+++ controllers/authController.js
@@ -32,20 +32,20 @@ const login = asyncHandler(async (req, res) => {
       },
     },
     process.env.ACCESS_TOKEN_SECRET,
-    { expiresIn: "10s" },
+    { expiresIn: "15m" },
   );
 
   const refreshToken = jwt.sign(
     { username: foundUser.username },
     process.env.REFRESH_TOKEN_SECRET,
-    { expiresIn: "1d" },
+    { expiresIn: "7d" },
   );
 
   // Create secure cookie with refresh token
   res.cookie("jwt", refreshToken, {
     httpOnly: true, //accessible only by web server
-    secure: true, //https
-    sameSite: "None", //cross-site cookie
+    secure: false, //https
+    sameSite: "Lax", //cross-site cookie
     maxAge: 7 * 24 * 60 * 60 * 1000, //cookie expiry: set to match rT
   });
 
@@ -96,7 +96,7 @@ const refresh = async (req, res) => {
 const logout = async (req, res) => {
   const cookies = req.cookies;
   if (!cookies?.jwt) return res.sendStatus(204); //No content
-  res.clearCookie("jwt", { httpOnly: true, sameSite: "None", secure: true });
+  res.clearCookie("jwt", { httpOnly: true, sameSite: "Lax", secure: false });
   res.json({ message: "Cookie cleared" });
 };
 
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,3 +8,5 @@ router.route("/").post(loginLimiter, authController.login);
 router.route("/refresh").get(authController.refresh);
 
 router.route("/logout").post(authController.logout);
+
+module.exports = router;
