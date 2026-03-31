b3078291b7c7858aa5a7d9465ad611441e9dc906
NifemiSoneye <nifemisoneye@gmail.com>
2026-03-31 10:38:18 -0700

    Updated allowed origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,6 @@
-const allowedOrigins = ["https://technotes-0n88.onrender.com/"];
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://technotes-0n88.onrender.com",
+];
 
 module.exports = allowedOrigins;
